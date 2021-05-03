8d192c3c5832db60d6339c3a8bdf415ea419f293
steven0476 <ste7088@gmail.com>
2021-05-03 23:36:51 +0700

    M03 express js, install express library, express js server

---
+++ M03/M03.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(req, res){
+    res.send('Hi :) 181110476 Steven disini, Pemrograman Web Lanjutan')
+})
+
+app.listen(3000, function(){console.log('server sudah jalan di port 3000')})
