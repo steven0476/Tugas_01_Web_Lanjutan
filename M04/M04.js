fc14dc16bf86687f87a7d149cb1f78cfe5508bb4
steven0476 <ste7088@gmail.com>
2021-05-04 00:09:14 +0700

    M04 routing, method post, body-parse

---
+++ M04/M04.js
@@ -0,0 +1,43 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+
+// routing: rute /
+app.get('/', function(req, res){
+    res.send(
+        `
+            <html>
+                <body>
+                    <form action = "/nama" method="POST">
+                        <label>Siapa namamu?</label>
+                        <input name="nama"/>
+                        <button>Lanjut</button>
+                    </form>    
+                </body>
+            </html>
+        `
+    )
+    res.end()
+})
+
+// routing: rute /, method post
+app.post('/nama', function(req, res){
+    // body parse
+    console.log(req.body)
+    res.send(
+        `
+            <html>
+                <body>
+                    <p> 
+                        Hi, `+req.body.nama+`.
+                    </p>    
+                </body>
+            </html>
+        `
+    )
+    res.end()
+})
+
+app.listen(3000, function(){console.log('server sudah jalan di port 3000')})
